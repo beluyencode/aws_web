305261292f5c83d25820a4719517ed99af3f37fa
beluyencode <longlc956@gmail.com>
2023-02-23 16:08:16 +0700

    1

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          beluyencode
+          beluyencode.online
         </a>
       </header>
     </div>
